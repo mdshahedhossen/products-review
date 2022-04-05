8dd5b2cb1a9f04486d6b75b0bcb751df48ccdb9c
mdshaedhossen <shaheddiu23@gmail.com>
2022-04-05 14:04:50 +0600

    blogs

--- src/Components/Blogs/Blogs.js
+++ src/Components/Blogs/Blogs.js
@@ -4,6 +4,20 @@ import './Blogs.css'
 const Blogs = () => {
     return (
         <div className='box-container'>
+            <h1>Question Answer</h1>
+
+            <div className="box">
+                <h3>What is context Api?</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis vero in porro at consequatur libero, placeat nulla tempora deserunt animi quas reiciendis optio sit ab perferendis suscipit ullam modi iste.</p>
+            </div>
+            <div className="box">
+                <h3>What is Semantic Tag?</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis vero in porro at consequatur libero, placeat nulla tempora deserunt animi quas reiciendis optio sit ab perferendis suscipit ullam modi iste.</p>
+            </div>
+            <div className="box">
+                <h3>What is the diffrence betweens inline blog and inline element?</h3>
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis vero in porro at consequatur libero, placeat nulla tempora deserunt animi quas reiciendis optio sit ab perferendis suscipit ullam modi iste.</p>
+            </div>
             
         </div>
     );
