498451bf0a67840b5ee3bd45c41b3bd2f09a2603
mdshaedhossen <shaheddiu23@gmail.com>
2022-04-05 20:31:04 +0600

    final commit

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import './About.css'
 
 const About = () => {
     return (
-        <div>
-            <h1>On Process this page</h1>
+        <div className='about-container'>
+            <h1>On Process this page...</h1>
         </div>
     );
 };
